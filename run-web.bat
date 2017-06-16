dd215cdfdbceb52c110c634b42c5a9ae0cadb052
qusthuang <qusthuanglong@163.com>
2017-06-16 20:11:34 +0800

    windows启动命令

---
+++ run-web.bat
@@ -0,0 +1,21 @@
+﻿@echo off
+
+
+
+start "xbin-store-cloud-web-admin" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-admin/build/libs/xbin-store-cloud-web-admin-1.0.0.jar
+
+start "xbin-store-cloud-web-cart" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-cart/build/libs/xbin-store-cloud-web-cart-1.0.0.jar
+
+start "xbin-store-cloud-web-item" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-item/build/libs/xbin-store-cloud-web-item-1.0.0.jar
+
+start "xbin-store-cloud-web-notify" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-notify/build/libs/xbin-store-cloud-web-notify-1.0.0.jar
+
+start "xbin-store-cloud-web-order" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-order/build/libs/xbin-store-cloud-web-order-1.0.0.jar
+
+start "xbin-store-cloud-web-portal" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-portal/build/libs/xbin-store-cloud-web-portal-1.0.0.jar
+
+start "xbin-store-cloud-web-search" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-search/build/libs/xbin-store-cloud-web-search-1.0.0.jar
+
+start "xbin-store-cloud-web-sso" java -jar -Xms256m -Xmx256m  xbin-store-cloud-web-sso/build/libs/xbin-store-cloud-web-sso-1.0.0.jar
+pause
+
---
+++ run.bat
@@ -0,0 +1,31 @@
+﻿@echo off
+
+start "eureka" java -jar -Xms256m -Xmx256m xbin-store-cloud-eureka/build/libs/xbin-store-cloud-eureka-1.0.0.jar
+
+start "xbin-store-cloud-config" java -jar -Xms256m -Xmx256m xbin-store-cloud-config/build/libs/xbin-store-cloud-config-1.0.0.jar
+
+start "xbin-store-cloud-admin" java -jar -Xms256m -Xmx256m  xbin-store-cloud-admin/build/libs/xbin-store-cloud-admin-1.0.0.jar
+
+start "xbin-store-cloud-sleuth" java -jar -Xms256m -Xmx256m  xbin-store-cloud-sleuth/build/libs/xbin-store-cloud-sleuth-1.0.0.jar
+
+start "xbin-store-cloud-turbine" java -jar -Xms256m -Xmx256m  xbin-store-cloud-turbine/build/libs/xbin-store-cloud-turbine-1.0.0.jar
+
+start "xbin-store-cloud-service-admin" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-admin/build/libs/xbin-store-cloud-service-admin-1.0.0.jar
+
+start "xbin-store-cloud-service-cart" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-cart/build/libs/xbin-store-cloud-service-cart-1.0.0.jar
+
+start "xbin-store-cloud-service-item" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-item/build/libs/xbin-store-cloud-service-item-1.0.0.jar
+
+start "xbin-store-cloud-service-notify" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-notify/build/libs/xbin-store-cloud-service-notify-1.0.0.jar
+
+start "xbin-store-cloud-service-order" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-order/build/libs/xbin-store-cloud-service-order-1.0.0.jar
+
+start "xbin-store-cloud-service-portal" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-portal/build/libs/xbin-store-cloud-service-portal-1.0.0.jar
+
+start "xbin-store-cloud-service-search" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-search/build/libs/xbin-store-cloud-service-search-1.0.0.jar
+
+start "xbin-store-cloud-service-sso" java -jar -Xms256m -Xmx256m  xbin-store-cloud-service-sso/build/libs/xbin-store-cloud-service-sso-1.0.0.jar
+
+
+pause
+
